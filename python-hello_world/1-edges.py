e6456ee7ac9aaa2f15829dd0eb4e2995e4965ff0
Joel Ekeng <joelekeng23@gmail.com>
2023-07-16 02:01:29 +0000

    Printing using format function

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,6 +2,6 @@
 word = "Holberton"
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[-2:]))
-print("Middle word: {}".format(word[4]))
+print("Middle word: {}".format(word[1:-1]))
 
 
