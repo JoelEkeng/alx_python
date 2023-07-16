ea033eb3faf646abdbb7b0e05a8c2c33a06ba1b4
Joel Ekeng <joelekeng23@gmail.com>
2023-07-16 01:51:50 +0000

    Python file for printing a line of code

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+word = "Holberton"
+print("First#!/usr/bin/python3
+word = "Holberton"
+print("Firstters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+ters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+
---
+++ python-hello_world/README.md
@@ -0,0 +1,1 @@
+I am coding with python in the project
