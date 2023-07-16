e89073b238b28bfae17cd00bdf04334859909070
Joel Ekeng <joelekeng23@gmail.com>
2023-07-16 01:57:27 +0000

    Using the print function

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
-print("First#!/usr/bin/python3
-word = "Holberton"
-print("Firstters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
-ters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[4]))
+
 
