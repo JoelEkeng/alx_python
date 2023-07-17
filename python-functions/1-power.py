374617e15651f3f8822352f1c2c6862116b53612
Joel Ekeng <joelekeng23@gmail.com>
2023-07-17 18:48:27 +0000

    Power function

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-#pow = __import__('1-power').pow
+pow = __import__('1-power').pow
 
-def pow(a,b):
-    print(pow(a,b))
-
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+def pow(a, b):
+    return a**b
