c3fab77ee763fd31dda2fbaea12117ee922ee868
Joel Ekeng <joelekeng23@gmail.com>
2023-07-17 11:56:23 +0000

    Last digit updated with negative number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,9 +9,9 @@ last_digit = number % 10
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif number < 0:
-    digit = abs(number)%10
+    digit = abs(number) % 10
     digit = -digit
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, digit))      
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, digit))
 # elif last_digit < 6 and last_digit != 0:
 #     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
 else:
