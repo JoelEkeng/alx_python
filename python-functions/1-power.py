02be6fd13ef27a9f11c331c08309253d078089c2
Joel Ekeng <joelekeng23@gmail.com>
2023-07-20 09:28:27 +0000

    Added a README file

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-pow = __import__('1-power').pow
+# pow = __import__('1-power').pow
 
-def pow(a, b):
-    return a**b
+# def pow(a, b):
+#     return a**b
+
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
---
+++ python-functions/README.md
@@ -0,0 +1,1 @@
+This repo consits of python functions and shows how it works
