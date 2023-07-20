66eebb79c53fb7c556bb5aae0a2f0c1553cf22f4
Joel Ekeng <joelekeng23@gmail.com>
2023-07-20 09:38:18 +0000

    Updated the Power file

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -2,9 +2,5 @@
 
 # pow = __import__('1-power').pow
 
-# def pow(a, b):
-#     return a**b
-
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+def pow(a, b):
+    return a**b
