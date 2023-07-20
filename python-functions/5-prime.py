5d90888cd1156fa10e5658fc2aaf5c1c1035f1c5
Joel Ekeng <joelekeng23@gmail.com>
2023-07-20 12:20:21 +0000

    Prime Numbers

---
+++ python-functions/5-prime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#is_prime = __import__('5-prime').is_prime
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True    
+
+print(is_prime(17))
+print(is_prime(15))
+print(is_prime(-5))
+print(is_prime(0))
