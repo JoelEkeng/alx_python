88dac92115334e288b291b9ac198b508dca0004b
Joel Ekeng <joelekeng23@gmail.com>
2023-07-20 09:58:43 +0000

    Temperature Converter Function

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -5,10 +5,7 @@
 def convert_to_celsius(fahrenheit):
     Celsius = (fahrenheit - 32) * (5/9)
     if Celsius % 4 == 0:
-        if Celsius == 0:
-            return "{:.1f}".format(Celsius)
-        else:    
-            return "{:.0f}".format(Celsius)
+        return "{:.1f}".format(Celsius)
     if fahrenheit < 0:
         return "{:.2f}".format(Celsius)
     else:
