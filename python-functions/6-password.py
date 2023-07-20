bac367ddba7f7fe6f5bf330d04b3e2f25b905df6
Joel Ekeng <joelekeng23@gmail.com>
2023-07-20 12:49:58 +0000

    Password Validator

---
+++ python-functions/6-password.py
@@ -0,0 +1,29 @@
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    if " " in password:
+        return False
+    
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+
+    if not (has_uppercase and has_lowercase and has_digit):
+        return False
+         
+    else:
+        return True        
+
+
+print(validate_password("Password123"))
+print(validate_password("abc123"))
+print(validate_password("Password 123"))
+print(validate_password("password123"))
