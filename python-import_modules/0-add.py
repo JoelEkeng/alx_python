d3fa06c91110e9f955c3103764220d87580ab5a3
Joel Ekeng <joelekeng23@gmail.com>
2023-07-27 10:05:23 +0000

    Importing addition files and ensuring it does not run from the main script

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,14 @@
+#!?usr/bin/python3
 import add_0
 
 add = add_0.add
 
-a = 1
-b = 2
+def main():
+    a = 1
+    b = 2
+    
+    print ("a + b = ", add(a, b))
 
-print ("a + b = ", add(a, b))
+if __name__ == "__main__":
+    main()
+        
