537fc92ffeeafed6c03c62595eb021e5d0420dc6
Joel Ekeng <joelekeng23@gmail.com>
2023-07-31 18:23:16 +0000

    Safe division printing with debugging

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,17 +2,14 @@
 
 
 def safe_print_division(a, b):
-    while True:
-        a = 10
-        b = 2
-        try:
-            if b == 0:
-                print ("Cannot be divided by Zero.")
-                break
-        except:
-            result = a / b
-        finally:
-            result = a / b
-            print("Inside result: {}".format(result))
-            print("{:d} / {:d} = {}".format(a, b, result))
+    try:
+        if b == 0:
+            print ("Cannot be divided by Zero.")
             break
+    except ZeroDivisionError:
+            return None
+    finally:
+        result = a / b
+        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
+        break
