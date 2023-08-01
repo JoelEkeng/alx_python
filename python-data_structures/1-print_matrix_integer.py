50f52b2dda2e65cb127279ed797e713b55854479
Joel Ekeng <joelekeng23@gmail.com>
2023-08-01 22:06:00 +0000

    Printing matrix integers

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,15 +1,20 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == " ":
+    # Check if the matrix is empty
+    if not matrix:
         print()
         return
 
+    # Loop through each row in the matrix
     for row in matrix:
-        for i, elements in enumerate(row):
-            print("{:d}".format(elements), end="")
-
-            if i < len(matrix) - 1:
+        # Loop through each element in the row
+        for i, element in enumerate(row):
+            # Print the element with proper formatting
+            print("{:d}".format(element), end="")
+            
+            # Print a space unless it's the last element in the row
+            if i < len(row) - 1:
                 print(" ", end="")
-       
-        print()   
+        
+        # Move to the next line after printing all elements in the row
+        print()
