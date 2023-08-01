4e0f70a5d9833511860882d839ab1b9c3495a04a
Joel Ekeng <joelekeng23@gmail.com>
2023-08-01 12:50:34 +0000

    Matrix integers

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print ("{}".format(i))
+    for i, matrix in enumerate (matrix):
+        print ("{}".format(matrix))
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
