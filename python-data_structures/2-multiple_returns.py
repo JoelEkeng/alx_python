ef88c509499af4a336e4f389c8ca6938500e8db2
Joel Ekeng <joelekeng23@gmail.com>
2023-08-01 14:05:07 +0000

    Multiple returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    new_sent = list(sentence)
-
-
     if sentence == "":
         return None
-    else:
-        print("Length: {:d} - First character: {}".format(length, new_sent[0]))
+
+    length = len(sentence)
+
+    new_sent = list(sentence)
     
+    return (length, new_sent[0])
+     
