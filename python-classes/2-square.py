4e94fddd9f3dd4540502ac56d1581c59856401eb
Joel Ekeng <joelekeng23@gmail.com>
2023-08-07 13:24:24 +0000

    Finding the Area of the square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -25,7 +25,7 @@ class Square:
 
     def area(self):
         """" This is used to find the square of the area by multipling by 2"""
-        return self.__size ** 1      
+        return self.__size ** 2      
 
     """
     Sets the size of the square.
@@ -54,21 +54,3 @@ class Square:
     """
     def get_size(self):
         return self.__size
-
-
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
