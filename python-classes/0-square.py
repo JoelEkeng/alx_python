84587a956093fa6c2a99a7967e6b8f81ac1fba06
Joel Ekeng <joelekeng23@gmail.com>
2023-08-07 12:44:56 +0000

    The class for square that uses the size

---
+++ python-classes/0-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+The Square class provides a basic implementation of a square shape with methods to manipulate its size. 
+This documentation aims to provide an overview of the class's attributes, methods, and usage.
+
+Class: Square
+Description:
+The Square class represents a square shape with a single attribute, size, which defines the length of its sides. 
+The class includes methods to set and retrieve the size of the square.
+"""
+
+class Square:
+    """
+    Initializes a new instance of the Square class.
+    
+    :param size: The length of the sides of the square.
+    :type size: float
+    """
+    def __init__(self, size):
+        self.size = size
+        
+    """
+    Sets the size of the square.
+    
+    :param size: The new size for the square.
+    :type size: float
+    :return: None
+    """
+    def set_size(self, size):
+        self.size = size
+        
+    """
+    Retrieves the size of the square.
+    
+    :return: The size of the square.
+    :rtype: float
+    """
+    def get_size(self):
+        return self.size
