218e832c88d1b5e871bb8a11f19bf2c3a092d9d1
Joel Ekeng <joelekeng23@gmail.com>
2023-08-07 13:47:16 +0000

    Access and Update private with getters and setters

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -18,10 +18,6 @@ class Square:
     """
     def __init__(self, size=0):
         self.__size = size
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
     
     @property
     def size(self):    
@@ -37,20 +33,20 @@ class Square:
     
     @size.setter
     def size(self, value):
-        self.size = value
+        self.__size = value
 
         """
         This checks that the size is an integer and
         The value of size is not less than zer0
         """
-        if not isinstance(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
         """" This is used to find the square of the area by multipling by 2"""
-        return self.__size ** 1   
+        return self.__size ** 2
 
     """
     Retrieves the size of the square.
@@ -58,15 +54,4 @@ class Square:
     :return: The size of the square.
     :rtype: int
     """
-    
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)        
+ 
