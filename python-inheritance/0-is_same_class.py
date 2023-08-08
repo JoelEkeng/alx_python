33806371883849d77d89b52816300264db8077a3
Joel Ekeng <joelekeng23@gmail.com>
2023-08-08 12:13:48 +0000

    Checking if the object matches the class

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 
+"""
+The is_same_class functions accepts two functions obj and a_class. 
+This documentation aims to provide an overview of inheritance using isinstance, issubclas, type and super built in funciton.
 
+Function: is_same_class
+Description:
+The is_same_class gets an object and check if the class corespond. 
+
+Examples:
+        # >>> is_same_class(5, int)
+        True
+        # >>> is_same_class("hello", str)
+        True
+        # >>> is_same_class([], list)
+        True
+        # >>> is_same_class(5, float)
+        False
+"""
 def is_same_class(obj, a_class):
-    return type(obj) is a_class
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))        
+    """
+    This checks if the Obj and the classes matches.
+    If the class matches the object passed into the function, the it's returned'
+    If it does not match, nothing is returned.
+    """
+    return type(obj) is a_class
