25331beabff785af11224337c51b39c9ffa69a75
Joel Ekeng <joelekeng23@gmail.com>
2023-08-08 12:54:06 +0000

    Checking if the subclass is from a parent class

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,29 @@
+#/usr/bin/python3
+
+
+"""
+The is_same_class functions accepts two functions obj and a_class. 
+This documentation aims to provide an overview of inheritance using isinstance, issubclas, type and super built in funciton.
+
+Function: is_same_class
+Description:
+The is_same_class gets an object and check if the class corespond. 
+
+Examples:
+        # >>> is_same_class(5, int)
+        True
+        # >>> is_same_class("hello", str)
+        True
+        # >>> is_same_class([], list)
+        True
+        # >>> is_same_class(5, float)
+        False
+"""
+def inherits_from(obj, a_class):
+    
+    """
+    This checks if the Obj and the classes matches.
+    The isinstance function is used to check that the object matches the data type
+    and returns the output.
+    """
+    return issubclass(int, a_class) 
