30539a993db2a463e47d0899ba270579bf03ef22
Joel Ekeng <joelekeng23@gmail.com>
2023-08-08 13:01:13 +0000

    Checking if a subclass is from a parent class

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,34 +1,20 @@
-#!/usr/bin/python3
+"""
+Module: 2-inherits_from
+A function that checks if an object is an instance of a class that inherited
+(directly or indirectly) from the specified class.
 
 """
-The inherits_from function accepts two arguments: an object (obj) and a class (a_class).
-This documentation aims to provide an overview of inheritance using isinstance, issubclass,
-type, and the super built-in function.
 
-Function: inherits_from
-Description:
-    The inherits_from function checks if an object belongs to a class or a subclass.
 
-Parameters:
-    obj (object): The object to be checked.
-    a_class (class): The class to check against.
-
-Returns:
-    bool: True if the object is an instance of the class or a subclass; False otherwise.
-
-Examples:
-    >>> inherits_from(5, int)
-    True
-    >>> inherits_from("hello", str)
-    True
-    >>> inherits_from([], list)
-    True
-    >>> inherits_from(5, float)
-    False
-"""
 def inherits_from(obj, a_class):
     """
-    This function checks if the obj is an instance of the specified class or a subclass.
-    The isinstance function is used to perform the check, and the result is returned.
+    Args:
+        obj: The object to check.
+        a_class: The class to check
     """
-    return isinstance(type(obj), a_class)
+
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """
+    Returns:
+            True if the object is an instance of a_class; otherwise False.
+    """
