2262d299d001916b87bd6caef5b967675fc476af
Joel Ekeng <joelekeng23@gmail.com>
2023-08-10 18:15:35 +0000

    Raising an exception in the BaseGeometry class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,12 +1,35 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 
-"""
-Module: 3-base_geometry
-It contains an empty class call BaseGeometry
+# """
+# 3-base_geometry
+# It contains an empty class call BaseGeometry
 
-"""
+# """
+# class BaseGeometrymetaclass(type):
+#     """
+#         This is a meta class created to inform the behaviour of the BaseGeometry class.
+#         It's the base class for the BaseGeometry class
+#     """
+#     def __dir__(cls):
+#         """This is used to check the list of attributes in the dir magic class.
+#         It ensures that __init_subclass__ is not printed"""
+#         return [attribute for attribute in super.__dir__() if attribute != '__init_subclass__']
+# class BaseGeometry(metaclass=BaseGeometrymetaclass):
+#     """This is an empty class.
+#     The Pass keyword is used to print an empty line
+#     """
+#     pass
+
+#     def __dir__(cls):
+#         """This is used to check the list of attributes in the dir magic class.
+#         It ensures that __init_subclass__ is not printed"""
+#         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
 class BaseGeometry:
-    """This is an empty class.
-    The Pass keyword is used to print an empty line
-     """
-    pass
+    pass 
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
