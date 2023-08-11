ba73b1ff8f445ba4a65fcae7ddca2e57ea8298ec
Joel Ekeng <joelekeng23@gmail.com>
2023-08-11 19:32:05 +0000

    Update version

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        if not isinstance(int, height):
+        if isinstance(int, height):
             raise TypeError ('height must be an integer')
         self.__width = width
         self.__height = height
