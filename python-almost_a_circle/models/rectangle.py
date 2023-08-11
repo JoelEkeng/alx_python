84254828160d2d6b6c18d8e819edeb2377d608d9
Joel Ekeng <joelekeng23@gmail.com>
2023-08-11 22:09:11 +0000

    Rectangle class importing from model.base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,42 +1,90 @@
 from models.base import Base
 
-class Rectangle:
+class Rectangle(Base):
+    """
+    Represents a geometric rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the top-left corner of the rectangle.
+        y (int): The y-coordinate of the top-left corner of the rectangle.
+        id (int): An optional identifier for the rectangle.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new Rectangle object.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the top-left corner. Default is 0.
+            y (int, optional): The y-coordinate of the top-left corner. Default is 0.
+            id (int, optional): An optional identifier for the rectangle.
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
     
-    @set_width.setter
-    def set_width(self, width):
+    @property
+    def width(self):
+        """int: The width of the rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, width):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            width (int): The new width value.
+        """
         self.__width = width
 
     @property
-    def get_width(self, width):
-        return self.__width
+    def height(self):
+        """int: The height of the rectangle."""
+        return self.__height
+    
+    @height.setter
+    def height(self, height):
+        """
+        Set the height of the rectangle.
 
-    @set_height.setter
-    def set_height(self, height):
+        Args:
+            height (int): The new height value.
+        """
         self.__height = height
     
     @property
-    def get_height(self):
-        return self.__height
+    def x(self):
+        """int: The x-coordinate of the top-left corner."""
+        return self.__x
     
-    @set_x.setter
+    @x.setter
     def x(self, x):
+        """
+        Set the x-coordinate of the top-left corner.
+
+        Args:
+            x (int): The new x-coordinate value.
+        """
         self.__x = x
     
     @property
-    def get_x(self):
-        return self.__x
-
-    @set_y.setter
-    def set_y(self, y):
-        self.__y = y 
-    
-    @property
-    def get_y(self):
+    def y(self):
+        """int: The y-coordinate of the top-left corner."""
         return self.__y
+    
+    @y.setter
+    def y(self, y):
+        """
+        Set the y-coordinate of the top-left corner.
+
+        Args:
+            y (int): The new y-coordinate value.
+        """
+        self.__y = y
