32043b13d31345622e2199dbf4d2e34f9c04458d
Joel Ekeng <joelekeng23@gmail.com>
2023-08-11 22:46:27 +0000

    Update the Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,11 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+        self._validate_width()
+        self._validate_height()
+        self._validate_x()
+        self._validate_y()
     
     @property
     def width(self):
@@ -106,3 +111,31 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError ('{} must be > 0'.format(y))
         self.__y = y
+
+    def _validate_width(self):
+        """Validate the width attribute."""
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
+
+    def _validate_height(self):
+        """Validate the height attribute."""
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
+
+    def _validate_x(self):
+        """Validate the x attribute."""
+        if not isinstance(self.__x, int):
+            raise TypeError("x must be an integer")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
+
+    def _validate_y(self):
+        """Validate the y attribute."""
+        if not isinstance(self.__y, int):
+            raise TypeError("y must be an integer")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
