a9c533f4f2a624066f36a4c6c0892d07b728ede9
Joel Ekeng <joelekeng23@gmail.com>
2023-08-12 00:23:27 +0000

    The Square class importing from the model/rectangle class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,48 +3,45 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
-
-    """
-    Square class, a subclass of Rectangle.
-
-    This class represents a square shape and inherits attributes and methods
-    from the Rectangle class. It adds an additional 'size' attribute to the
-    Square objects.
-
-    Attributes:
-    - id (int): A unique identifier for the Square.
-    - x (int): The x-coordinate of the top-left corner of the Square.
-    - y (int): The y-coordinate of the top-left corner of the Square.
-    - size (int): The side length of the Square.
-
-    Methods:
-    - __init__(self, size, x=0, y=0, id=None): Constructor to initialize a Square object.
-    - __str__(self): Return a formatted string representation of the Square object.
-
-    Note: This class assumes that the Rectangle class is defined in 'models.rectangle'.
-    """
+    """This class represents a rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Constructor for the Square class.
+        """Initialize a square.
 
-        Parameters:
-        size (int): The side length of the square.
-        x (int): The x-coordinate of the top-left corner of the square. Default is 0.
-        y (int): The y-coordinate of the top-left corner of the square. Default is 0.
-        id (int): A unique identifier for the square. Default is None.
-
-        Note: The constructor initializes attributes 'id', 'x', 'y', and 'size'.
+        Args:
+            size: The size of the square.
+            x: The x-coordinate of the square.
+            y: The y-coordinate of the square.
+            id: The ID of the square.
         """
-        super().__init__(id, x, y, width, height)
-        self.size = width
-        self.size = height
+        super().__init__(size, size, x, y, id)
+        
     
     def __str__(self):
         """
-        Generate a formatted string representation of the Square.
+        Returns a string representation of the square.
 
         Returns:
-        str: A formatted string representing the Square object.
+            str: The formatted string containing the square information.
         """
-        return ('[Square] ({}) <{}>/<{}> - <{}>'.format(self.id, self.x, self.y, self.size))
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+    
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The size to be set.
+        """
+        self.width = self.height = value
