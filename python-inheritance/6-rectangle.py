94a75116f78725b3d6c6b3a7fa2ca24b77b32bb9
Joel Ekeng <joelekeng23@gmail.com>
2023-08-12 00:39:32 +0000

    Rectangle class update

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,68 +1,80 @@
-#!/usr/bin/python3
-
 """
-3-base_geometry
-It contains an empty class call BaseGeometry
+This module defines the Rectangle class.
 
+The Rectangle class inherits from the BaseGeometry class. It has two private attributes: width and height. These attributes are validated by the integer_validator() method from the BaseGeometry class.
+
+The Rectangle class has an area() method that calculates the area of the rectangle.
 """
-class BaseGeometrymetaclass(type):
-    """
-        This is a meta class created to inform the behaviour of the BaseGeometry class.
-        It's the base class for the BaseGeometry class
-    """
-    def __dir__(cls):
-        """This is used to check the list of attributes in the dir magic class.
-        It ensures that __init_subclass__ is not printed"""
-        return [attribute for attribute in super.__dir__() if attribute != '__getstate__']
-class BaseGeometry(metaclass=BaseGeometrymetaclass):
-    """This is an empty class.
-    The Pass keyword is used to print an empty line
-    """
-    pass
+
+class OverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
 
     def __dir__(cls):
-        """This is used to check the list of attributes in the dir magic class.
-        It ensures that __init_subclass__ is not printed"""
-        return [attribute for attribute in super().__dir__() if attribute != '__getstate__']
-
-    def area(self):
-        """It's used to raise an exception that the area method is not implemented
         """
-        raise Exception ('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Creating an instance of name and value
+        Returns:
+            list: List of attributes excluding __init_subclass__.
         """
-        self.name = name
-        self.value = value
-        
-        """Checking that the value passed by the user is an Integer
-        If it's not an Integer, a TypeError is raised
-        """
-        if not isinstance(value, int):
-            raise TypeError ('{} must be an integer'.format(name))
-        """Checking that the value must be great than zero
-        if the value is less than or equal to zero, then a valueError is raised.
-        """
-        if value <= 0 :
-            raise ValueError ('{} must be greater than 0'.format(name))
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
-    The Rectangle class inherits from the BaseGeometry class
+    This class defines a rectangle.
+
+    Attributes:
+        _width (int): The width of the rectangle.
+        _height (int): The height of the rectangle.
+
+    Methods:
+        area(): Calculates the area of the rectangle.
+
+    Raises:
+        AttributeError: If the attribute width or height is accessed.
+        TypeError: If the value of the attribute width or height is not an integer.
+        ValueError: If the value of the attribute width or height is less than or equal to 0.
     """
+
     def __init__(self, width, height):
-        """The intneger_validator which is an instance of the BaseGeometry class is called
         """
-        BaseGeometry.integer_validator(self, width, height)
-        """Here an Instance of width and height is created
+        Initializes a new rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If the value of the attribute width or height is not an integer.
+            ValueError: If the value of the attribute width or height is less than or equal to 0.
         """
+        super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-    
+        self._width = width
+        self._height = height
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self._width * self._height
+
+    # Prevents the attributes width and height from being accessed directly.
+    def __getattribute__(self, name):
+        if name in ["_width", "_height"]:
+            raise AttributeError("'Rectangle' object has no attribute '{}'".format(name))
+        elif name == "height" and not isinstance(object.__getattribute__(self, "_height"), int):
+            raise TypeError("height must be an integer")
+        return super().__getattribute__(name)
+
     def __dir__(cls):
-        """This is used to check the list of attributes in the dir magic class.
-        It ensures that __init_subclass__ is not printed"""
-        return [attribute for attribute in super().__dir__() if attribute != '__getstate__']
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
