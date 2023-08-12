fe7cc0510c7d041c0d986966faeb8bc6e0b62bff
Joel Ekeng <joelekeng23@gmail.com>
2023-08-12 01:00:03 +0000

    Empty BaseGeometryclass

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -5,7 +5,7 @@
 It contains an empty class call BaseGeometry
 
 """
-class BaseGeometrymetaclass(type):
+class OverrideMetaClass(type):
     """
         This is a meta class created to inform the behaviour of the BaseGeometry class.
         It's the base class for the BaseGeometry class
@@ -14,7 +14,7 @@ class BaseGeometrymetaclass(type):
         """This is used to check the list of attributes in the dir magic class.
         It ensures that __init_subclass__ is not printed"""
         return [attribute for attribute in super.__dir__() if attribute != '__init_subclass__']
-class BaseGeometry(metaclass=BaseGeometrymetaclass):
+class BaseGeometry(OverrideMetaClass):
     """This is an empty class.
     The Pass keyword is used to print an empty line
     """
