257152a587f7c8e994fa825791b788b563638f2b
Joel Ekeng <joelekeng23@gmail.com>
2023-08-22 17:58:08 +0000

    Listing all states from hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,35 @@
-import MySQLdb
 import sys
+import MySQLdb
 
 def main():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
+    connection_params = {
+        "host": "localhost",
+        "user": username,
+        "passwd": password,
+        "db": database,
+        "port": 3306,
+    }
 
-    cur = db.cursor()
+    try:
+        db = MySQLdb.connect(**connection_params)
+        cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+        cur.execute("SELECT * FROM states ORDER BY states.id")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-    cur.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
 
 if __name__ == "__main__":
     main()
