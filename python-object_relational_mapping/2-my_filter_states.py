f30a3f96aefe0df577e5609ce28e2552bcc518e5
Joel Ekeng <joelekeng23@gmail.com>
2023-08-23 16:37:02 +0000

    FIltering states based on user response

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ def main():
         query = (
             "SELECT * FROM states WHERE BINARY name LIKE"
             "'N%' ORDER BY states.id ASC"
-        )
+        ).format(argv)
         cur.execute(query)
         rows = cur.fetchall()
         for row in rows:
