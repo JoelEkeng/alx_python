5663f5d255fb6449b2aaaac4bfad3cc0f59d9b6d
Joel Ekeng <joelekeng23@gmail.com>
2023-08-24 11:08:53 +0000

    Avoiding SQL Injections through user input

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -24,7 +24,7 @@ def main():
         query = (
             "SELECT * FROM states WHERE BINARY name LIKE"
             "'{}' ORDER BY states.id ASC"
-        ).format(state_name, state)
+        ).format(state_name)
 
         cur.execute(query)
 
