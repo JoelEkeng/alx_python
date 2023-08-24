2e11ba1d0231582c145d328f119dcd7159b279c5
Joel Ekeng <joelekeng23@gmail.com>
2023-08-24 09:36:16 +0000

    Filtering states by users choice

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,7 @@
 import sys
 import MySQLdb
 
-
-def main():
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state = sys.argv[4]
-
+def search_states_by_name(username, password, database, state_name):
     connection_params = {
         "host": "localhost",
         "user": username,
@@ -18,24 +12,31 @@ def main():
 
     try:
         db = MySQLdb.connect(**connection_params)
-        cur = db.cursor()
+        cursor = db.cursor()
 
-        query = (
-            "SELECT * FROM states WHERE BINARY name LIKE"
-            "'N%' ORDER BY states.id ASC".format(argv)
-        )
-        cur.execute(query, (state))
-        rows = cur.fetchall()
+        query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+        cursor.execute(query, (state_name,))
+
+        rows = cursor.fetchall()
+
         for row in rows:
             print(row)
 
     except MySQLdb.Error as e:
         print("Error:", e)
     finally:
-        if cur:
-            cur.close()
+        if cursor:
+            cursor.close()
         if db:
             db.close()
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+
+        search_states_by_name(username, password, database, state_name)
