0c3f59e8a32a64ffba0bbc8e243283ff911ce8eb
Joel Ekeng <joelekeng23@gmail.com>
2023-08-24 18:37:12 +0000

    Creating a base class base on the sqlalchemy function

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+from sqlalchemy import column, integer, string
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """
+        Maps to the 'states' table.
+
+        Attributes:
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
+
+        Args:
+        Base (DeclarativeMeta): The base class.
+    """
+    
+    __tablename__ = 'states'
+
+    id = column(integer, primary_key=True, autoincrement=True, nullable=False)
+    name = column(String(128), nullable=False)
