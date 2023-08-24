28e1fb990f734c49f7ab21b8df63e482d48bd656
Joel Ekeng <joelekeng23@gmail.com>
2023-08-24 18:43:43 +0000

    Creating a base class base on the sqlalchemy function

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,5 +17,4 @@ class State(Base):
         """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    
+    name = Column(String(128), nullable=False)    
