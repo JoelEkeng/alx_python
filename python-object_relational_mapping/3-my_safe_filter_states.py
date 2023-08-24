181d0656fa4eba3e53a28bdd8637c0fbb08ce711
Joel Ekeng <joelekeng23@gmail.com>
2023-08-24 10:13:25 +0000

    Avoiding SQL Injections through user input

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,44 @@
+import sys
+import MySQLdb
+
+
+def main():
+    username = input("Username: ")
+    password = input("Password: ")
+    database = input("Database: ")
+    state_name = input("State name: ")
+
+    connection_params = {
+        "host": "localhost",
+        "user": username,
+        "passwd": password,
+        "db": database,
+        "port": 3306,
+    }
+
+    try:
+        db = MySQLdb.connect(**connection_params)
+        cur = db.cursor()
+
+        query = (
+            "SELECT * FROM states WHERE BINARY name LIKE"
+            "'{}' ORDER BY states.id ASC"
+        ).format(state_name)
+
+        cur.execute(query)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+if __name__ == "__main__":
+    main()
