eabeb410da706dc11a0767ccb7e8bb751be5fa8e
Joel Ekeng <joelekeng23@gmail.com>
2023-08-24 12:05:26 +0000

    Script to order cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,45 @@
+import sys
+import MySQLdb
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection_params = {
+        "host": "localhost",
+        "user": username,
+        "passwd": password,
+        "db": database,
+        "port": 3306,
+    }
+
+    try:
+        db = MySQLdb.connect(**connection_params)
+        cur = db.cursor()
+
+        query = """
+        SELECT *
+        FROM cities
+        WHERE name
+        LIKE BINARY %s
+        ORDER BY cities.id ASC
+        """
+        cur.execute(query)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+if __name__ == "__main__":
+    main()
